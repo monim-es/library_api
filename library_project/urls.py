693291fb0b0c682f2ed66a916cd4b06326fa5392
momo <monimsrw@outlook.fr>
2025-10-16 19:36:12 +0100

    add swagger api

--- library_project/urls.py
+++ library_project/urls.py
@@ -5,6 +5,22 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Library Management API",
+      default_version='v1',
+      description="API documentation for the Library Management System",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -15,4 +31,7 @@ urlpatterns = [
     # App endpoints
     path('api/', include('users.urls')),
     path('api/', include('books.urls')),
+    # swagger endpoints 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
