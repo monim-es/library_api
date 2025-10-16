28a0c5c26558ed085f46cf038911116069bc0d9f
momo <monimsrw@outlook.fr>
2025-10-16 16:40:26 +0100

    adding the profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    """User profile extending the built-in User model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
